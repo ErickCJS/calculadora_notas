b66c65a1ccc8e315e24263c9da5eeb11b281d925
ErickCJS <julonerick1@gmail.com>
2025-01-10 23:03:59 -0500

    sa

--- conexion.py
+++ conexion.py
@@ -5,6 +5,5 @@ def obtener_conexion():
         host= 'calculador11-dsjulonerick-7a5a.l.aivencloud.com',
         port=13930,
         user= 'avnadmin',
-        password= 'AVNS_BIpa-YQYdIrYqo0tK2u',
         db='calculadora' 
     )
